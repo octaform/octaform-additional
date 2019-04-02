cbe20f5db06086b5226c6bc95b1829644bc21600
Christian Fortes <christian.fortes@iwsbrazil.com>
2019-04-02 19:05:35 -0300

    Build changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.APP_ENV === "production";
 const config = require(`./webpack/${isProd ? 'prod' : 'dev'}.config.js`);
 
 module.exports = config;
